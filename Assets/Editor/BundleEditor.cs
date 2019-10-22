820b376d281aa5fc0dd6bddae5ade15fd9f53427
zgzcir <zgzcir@outlook.com>
2019-10-22 14:33:39 +0800

    init

---
+++ Assets/Editor/BundleEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class BundleEditor
+{
+    [MenuItem("Tools/打包")]
+    public static void Build()
+    {
+        BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.ChunkBasedCompression,
+            EditorUserBuildSettings.activeBuildTarget);
+        AssetDatabase.Refresh();
+        Debug.Log("Build AssetBundle Done");
+    }
+
+}
---
+++ Assets/Scripts/ResourceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ResourceLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        AssetBundle ab=    AssetBundle.LoadFromFile(Application.streamingAssetsPath + "/attack");
+        GameObject go = Instantiate(ab.LoadAsset<GameObject>("attack"));
+        Instantiate(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/GameData/Prefabs/Attack.prefab"));
+        
+    }
+    
+    
+    
+}
