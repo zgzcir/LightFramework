e708109c47deac885148cc5ee159ef1b128f656c
zgzcir <zgzcir@outlook.com>
2019-11-23 14:36:36 +0800

    window事件管理

--- Assets/Scripts/UGUI/Window.cs
+++ Assets/Scripts/UGUI/Window.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Window
@@ -14,7 +17,6 @@ public class Window
 
     public virtual void Awake(params object[] paramList)
     {
-        
     }
 
     public virtual void OnShow(params object[] paramList)
@@ -23,7 +25,6 @@ public class Window
 
     public virtual void OnDisable()
     {
-                
     }
 
     public virtual void OnUpdate()
@@ -32,10 +33,68 @@ public class Window
 
     public virtual void OnClose()
     {
+        RemoveAllButtonListener();
+        RemoveAllToggleListener();
+
+        buttons.Clear();
+        toggles.Clear();
     }
 
     public virtual bool OnMessage(UIMessageType messageType, params object[] paramList)
     {
         return true;
     }
+
+
+    #region events
+
+    public void RemoveAllButtonListener()
+    {
+        buttons.ForEach(b => b.onClick.RemoveAllListeners());
+    }
+
+    public void RemoveAllToggleListener()
+    {
+        toggles.ForEach(t => t.onValueChanged.RemoveAllListeners());
+    }
+
+    public void AddButtonClickListener(Button button, UnityAction unityAction)
+    {
+        if (button != null)
+        {
+            if (!buttons.Contains(button))
+            {
+                buttons.Add(button);
+            }
+
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(unityAction);
+            button.onClick.AddListener(PlayButtonSound);
+        }
+    }
+
+    protected virtual void PlayButtonSound()
+    {
+    }
+
+    public void AddToggleListener(Toggle toggle, UnityAction<bool> unityAction)
+    {
+        if (toggle != null)
+        {
+            if (!toggles.Contains(toggle))
+            {
+                toggles.Add(toggle);
+            }
+
+            toggle.onValueChanged.RemoveAllListeners();
+            toggle.onValueChanged.AddListener(unityAction);
+            toggle.onValueChanged.AddListener(PlayToggleSound);
+        }
+    }
+
+    public virtual void PlayToggleSound(bool isOn)
+    {
+    }
+
+    #endregion
 }
